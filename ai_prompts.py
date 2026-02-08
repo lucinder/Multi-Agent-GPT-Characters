e1df16e3640fd52ac9120228f9b4527fd679d234
lucinder <savannawh@gmail.com>
2026-02-08 12:08:52 -0800

    Fill out random traits lib and add D&D-style character generation.

--- ai_prompts.py
+++ ai_prompts.py
@@ -65,26 +65,58 @@ AGENT_6 = {"role": "system", "content": f'''
     {SYSTEM_OUTRO}
 '''}
 
+import random, json
+
+traits_lib = {}
+with open("random_traits_lib.json","r") as f:
+    traits_lib = json.load(f)
+
 def generate_random_agent_prompt(name) -> str:
     """Generate a random agent prompt using traits from the traits library."""
     agent_prompt = ""
-    import random, json
-    traits_lib = {}
-    with open("random_traits_lib.json","r") as f:
-        traits_lib = json.load(f)
-    dnd_class = random.choice(traits_lib["CLASSES_DND"])
-    species = random.choice(traits_lib["SPECIES_DND"])
     adjective = random.choice(list(traits_lib["ADJECTIVES"].keys()))
     role = random.choice(list(traits_lib["ROLES"].keys()))
-    introduction = f"You are {name}, a {species} {dnd_class}. In this conversation, your role is the {adjective} {role}."
+    goal = traits_lib["ROLES"][role]["GOAL"]
+    introduction = f"You are {name}. In this conversation, your role is the {adjective} {role}. Your goal is to {goal}"
     agent_prompt += introduction + "\n\n"
     agent_prompt += "Traits and Behaviors:\n"
-    traits = traits_lib["ROLES"][role]
-    for trait in traits:
-        agent_prompt += f"- {trait}\n"
-    traits = traits_lib["ADJECTIVES"][adjective]
+    traits = traits_lib["ADJECTIVES"][adjective] + traits_lib["ROLES"][role]["TRAITS"]
     for trait in traits:
         agent_prompt += f"- {trait}\n"
     extra_trait = random.choice(traits_lib["FLAVOR_TRAITS"])
     agent_prompt += f"- {extra_trait}\n"
+    return agent_prompt
+
+def generate_random_agent_prompt_dnd(name, is_animal_fantasy=False) -> str:
+    """Generate a random agent prompt for agentic D&D players."""
+    agent_prompt = ""
+    dnd_class = random.choice(list(traits_lib["CLASSES_DND"].keys()))
+    is_caster = dnd_class in ["Wizard","Sorcerer","Druid","Cleric","Bard","Paladin","Ranger","Warlock","Artificer"]
+    species = random.choice(list(traits_lib["SPECIES_DND" if not is_animal_fantasy else "SPECIES_ANIMAL"].keys()))
+    species_trait = traits_lib["SPECIES_DND" if not is_animal_fantasy else "SPECIES_ANIMAL"][species]
+    if species in ["BEAR","CANINE","FELINE","LIZARD","BIRD","FISH","INSECT","UNGULATE"]:
+        species = random.choice(traits_lib["RANDOM_" + species])
+    adjective = random.choice(list(traits_lib["ADJECTIVES"].keys()))
+    role = random.choice(list(traits_lib["ROLES"].keys()))
+    goal = traits_lib["ROLES"][role]["GOAL"]
+    introduction = f"You are {name}, a {species} {dnd_class}. In this adventure, your role is the {adjective} {role}. Your goal is to {goal}"
+    agent_prompt += introduction + "\n\n"
+    # Speech + Traits
+    agent_prompt += "Traits and Behaviors:\n"
+    traits = traits_lib["ADJECTIVES"][adjective] + traits_lib["ROLES"][role]["TRAITS"]
+    for trait in traits:
+        agent_prompt += f"- {trait}\n"
+    extra_trait = random.choice(traits_lib["FLAVOR_TRAITS"])
+    agent_prompt += f"- {extra_trait}\n\n"
+    # Mechanical abilities
+    class_features = traits_lib["CLASSES_DND"][dnd_class]
+    agent_prompt += "Special Abilities:\n"
+    for feature in class_features["FEATURES"]:
+        agent_prompt += f"- {feature}\n"
+    agent_prompt += "You can use each of your special abilities once per encounter" + ("." if not is_caster else ", except for Spellcasting. You have a well of magic energy and can use Spellcasting until this well is depleted. At the end of each day, your magic energy replenishes.") + "\n\n"
+    # Spell list
+    if is_caster:
+        agent_prompt += "Spell List:\n"
+        for spell in class_features["SPELLS"]:
+            agent_prompt += f"- {spell}\n"
     return agent_prompt
